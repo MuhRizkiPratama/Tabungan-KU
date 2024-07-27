3e9278c23d1cfd3f426223d9c545a24a35ad1c80
Muh Rizki Pratama <rizkipratama0550@gmail.com>
2024-07-27 23:57:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Remix Icon -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+
+    <!-- Helvetica Font -->
+    <link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet">
+
+    <!-- Montserrat Font  -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+    <title>TABUNGAN KU</title>
+</head>
+<body>
+    <!-- Navbar Start-->
+    <nav>
+        <div class="logo">
+            <div class="firstname">TABUNGAN</div>
+            <div class="lastname">KU</div>
+        </div>
+        <div class="menu-icon" id="menu-icon">
+            <i class="ri-menu-line"></i>
+        </div>
+        <ul class="menu-list hidden" id="menu-list">
+            <li><a href="/">Home</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Content Start -->
+    <main>
+        
+    </main>
+    <!-- Content End -->
+
+    <footer>
+        <p>Copyright 2024. By Rizki Pratama</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
